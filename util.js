dc34bbc41518007241818bcf075f8e7e72545d43
Itenga <msukuma@protonmail.com>
2018-11-17 15:05:20 +0300

    adjlist using str => set k => v pairs

--- util.js
+++ util.js
@@ -5,20 +5,24 @@ const Edge = require('./edge');
 function loadGraph(data) {
   const adjList = new Map();
   const edges = new Set();
+  let set;
 
   data.split(', ')
       .forEach(edge => {
         edge = edge.split('');
         let [from, to, distance] = edge;
         distance = parseInt(distance);
+
         edge = new Edge(from, to, distance);
 
         edges.add(edge);
 
         if (adjList.has(from)) {
-          adjList.get(from).push(edge);
+          adjList.get(from).add(edge);
         } else {
-          adjList.set(from, [edge]);
+          set = new Set();
+          set.add(edge);
+          adjList.set(from, set);
         }
       });
 
