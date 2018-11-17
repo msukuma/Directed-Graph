3dfa14181446b924f4e67546344926dcd1cb4a64
Itenga <msukuma@protonmail.com>
2018-11-17 13:14:12 +0300

    base graph class

---
+++ graph.js
@@ -0,0 +1,9 @@
+const { loadGraph } = require('util');
+
+module.exports = class Graph {
+  constructor(data) {
+    const { adjList, edges } = loadGraph(data);
+    this.adjList = adjList;
+    this.edges = edges;
+  }
+};
