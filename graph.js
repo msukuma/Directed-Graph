8d233d9289de469c9a418d5bee4f6faf339b75be
Itenga <msukuma@protonmail.com>
2018-11-17 14:07:19 +0300

    graph - hasNode + tests

--- graph.js
+++ graph.js
@@ -6,4 +6,24 @@ module.exports = class Graph {
     this.adjList = adjList;
     this.edges = edges;
   }
+
+  hasNodes(nodes) {
+    let node;
+
+    if (!(nodes instanceof Array)) {
+      throw new Error('nodes must be an Array of Strings of length 1 (i.e. char)');
+    }
+
+    for (var i = 0; i < nodes.length; i++) {
+      node = nodes[i];
+
+      if (typeof node !== 'string' || node.length !== 1) {
+        throw new Error('a node must be a string of length 1 (i.e. char)');
+      }
+
+      if (!this.adjList.has(node)) { return false; }
+    }
+
+    return true;
+  }
 };
--- test/graph.js
+++ test/graph.js
@@ -3,10 +3,10 @@ const { assert, expect } = require('chai');
 const { DATA } = require('./test-constants');
 
 describe('Graph', () => {
-  describe('constructor', () => {
-    let graph;
+  let graph;
+  before(() => graph = new Graph(DATA));
 
-    before(() => graph = new Graph(DATA));
+  describe('constructor', () => {
 
     it('should create a graph instance', () => {
       expect(graph).to.be.an.instanceof(Graph);
@@ -18,6 +18,30 @@ describe('Graph', () => {
 
     it('should create a graph with an edges property', () => {
       expect(graph).to.have.own.property('edges');
+    });
+  });
+
+  describe('hasNodes', () => {
+    it('method exists', () => {
+      expect(graph.hasNodes).to.be.a('function');
+    });
+
+    it('validates input is an Array', () => {
+      const badCall = () => graph.hasNodes('A-B-C');
+      expect(badCall).to.throw('Array');
+    });
+
+    it('validates input is an Array of 1 char strings', () => {
+      const badCall = () => graph.hasNodes(['A', 'B', {}]);
+      expect(badCall).to.throw('string');
+    });
+
+    it('return true if graph has all input nodes', () => {
+      expect(graph.hasNodes(['A', 'B', 'C'])).to.equal(true);
+    });
+
+    it('return true if graph does not have all input nodes', () => {
+      expect(graph.hasNodes(['A', 'B', 'F'])).to.equal(false);
     });
   });
 
